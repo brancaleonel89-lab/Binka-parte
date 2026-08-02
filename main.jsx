618cc5b810f97ec9f0ab612496d704fce498aa28
brancaleonel89-lab <branca.leonel89@gmail.com>
2026-08-02 10:08:38 -0300

    Wrap App component with AuthGate for authentication

--- main.jsx
+++ main.jsx
@@ -1,11 +1,14 @@
-import "./storage.js"; // Inicializar Supabase antes que todo
+import "./storage.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthGate } from "./Auth";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthGate>
+      {({ isAdmin, user }) => <App isAdmin={isAdmin} userEmail={user.email} />}
+    </AuthGate>
   </React.StrictMode>
 );
